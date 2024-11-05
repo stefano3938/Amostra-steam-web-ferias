bbfadec3b2f742ead447cd1279aaaf2b408fc6d2
stefano3938 <sasukeuchihaps338@gmail.com>
2024-11-04 23:24:42 -0300

    mudança na estrutura de transiçao de botoes
    ao invez de ser redirecionado ao cite apos clicar nos botoes, voce é redirecionado a pagina html referente ao que voce clicou. porem precisei tirar os arqwuivos das pastas para isso. pretendo futuramente organizar os elementos de cada arquivo para ficar melhor a visualizaçao. tambem mudei o nome do documento de todos para oasis e adicnei icon

---
+++ pagina de login/criar-senha.js
@@ -0,0 +1,17 @@
+let imagemTermos = document.getElementById('imagem-termos');
+let botaoTermos = document.getElementById('botao-termos');
+let contador = 1;
+
+function Termos(){
+ if(contador == 1){
+    imagemTermos.src = 'v.svg';
+contador = 0;
+}
+else
+{
+imagemTermos.src = 'a';
+contador = 1;
+}
+}
+
+botaoTermos.addEventListener('click', Termos);
---
+++ pagina de login/login.js
@@ -0,0 +1,18 @@
+function limparTexto(){document.getElementById("email").value="",document.getElementById("senha").value=""}
+let caixaMarcar = document.getElementById('botao-lembra-de-mim');
+let botal = document.getElementById('botao');
+let contador = 1;
+function caixinha()
+{
+ if(contador == 1){
+    caixaMarcar.src = 'botao-marcar.svg';
+contador = 0;
+}
+else
+{
+caixaMarcar.src = 'botao-marcar-falso.svg';
+contador = 1;
+}
+}
+
+ botao.addEventListener('click', caixinha);
