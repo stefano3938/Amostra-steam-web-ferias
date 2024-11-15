ea9868c6db28b558fcf456d208f6bd05bb1cea8b
stefano3938 <sasukeuchihaps338@gmail.com>
2024-11-15 14:29:54 -0300

    organizei as coisas em pastas
    precisei escrever ../ no começo de cada arquivo img pos estavam dentro de pastas diferentes

--- pagina de login/pagina home/home.js
+++ pagina de login/pagina home/home.js
@@ -17,21 +17,21 @@ document.addEventListener('DOMContentLoaded', () => {
         p1BotaoMudarImagem2.style.backgroundColor = '#5e0b37';
         p1BotaoMudarImagem1.style.backgroundColor = 'white';
         p1BotaoMudarImagem3.style.backgroundColor = 'white';
-        imagemPrimeiraPagina.src = 'imagens/imagem-primeira-pagina2.png';
+        imagemPrimeiraPagina.src = '../imagens/imagem-primeira-pagina2.png';
     }
 
     function trocarImagem1() {
         p1BotaoMudarImagem1.style.backgroundColor = '#5e0b37';
         p1BotaoMudarImagem2.style.backgroundColor = 'white';
         p1BotaoMudarImagem3.style.backgroundColor = 'white';
-        imagemPrimeiraPagina.src = 'imagens/imagem-primeira-pagina.png';
+        imagemPrimeiraPagina.src = '../imagens/imagem-primeira-pagina.png';
     }
 
     function trocarImagem3() {
         p1BotaoMudarImagem3.style.backgroundColor = '#5e0b37';
         p1BotaoMudarImagem1.style.backgroundColor = 'white';
         p1BotaoMudarImagem2.style.backgroundColor = 'white';
-        imagemPrimeiraPagina.src = 'imagens/imagem-primeira-pagina3.png';
+        imagemPrimeiraPagina.src = '../imagens/imagem-primeira-pagina3.png';
     }
 
     p1BotaoMudarImagem2.addEventListener('click', trocarImagem2);
@@ -41,23 +41,23 @@ document.addEventListener('DOMContentLoaded', () => {
     function p3TrocarImagem1() {
         p3BotaoMudarImagem1.style.backgroundColor = '#5e0b37';
         p3BotaoMudarImagem2.style.backgroundColor = 'white';
-        pagina3Imagem1.src = 'imagens/pagina3-imagem1.svg';
-        pagina3Imagem2.src = 'imagens/pagina3-imagem2.svg';
-        pagina3Imagem3.src = 'imagens/pagina3-imagem3.svg';
-        pagina3Imagem4.src = 'imagens/pagina3-imagem4.svg';
-        pagina3Imagem5.src = 'imagens/pagina3-imagem5.svg';
-        pagina3Imagem6.src = 'imagens/pagina3-imagem6.svg';
+        pagina3Imagem1.src = '../imagens/pagina3-imagem1.svg';
+        pagina3Imagem2.src = '../imagens/pagina3-imagem2.svg';
+        pagina3Imagem3.src = '../imagens/pagina3-imagem3.svg';
+        pagina3Imagem4.src = '../imagens/pagina3-imagem4.svg';
+        pagina3Imagem5.src = '../imagens/pagina3-imagem5.svg';
+        pagina3Imagem6.src = '../imagens/pagina3-imagem6.svg';
     }
 
     function p3TrocarImagem2() {
         p3BotaoMudarImagem1.style.backgroundColor = 'white';
         p3BotaoMudarImagem2.style.backgroundColor = '#5e0b37';
-        pagina3Imagem1.src = 'imagens/pagina3-imagem7.svg';
-        pagina3Imagem2.src = 'imagens/pagina3-imagem8.svg';
-        pagina3Imagem3.src = 'imagens/pagina3-imagem9.svg';
-        pagina3Imagem4.src = 'imagens/pagina3-imagem10.svg';
-        pagina3Imagem5.src = 'imagens/pagina3-imagem11.svg';
-        pagina3Imagem6.src = 'imagens/pagina3-imagem12.svg';
+        pagina3Imagem1.src = '../imagens/pagina3-imagem7.svg';
+        pagina3Imagem2.src = '../imagens/pagina3-imagem8.svg';
+        pagina3Imagem3.src = '../imagens/pagina3-imagem9.svg';
+        pagina3Imagem4.src = '../imagens/pagina3-imagem10.svg';
+        pagina3Imagem5.src = '../imagens/pagina3-imagem11.svg';
+        pagina3Imagem6.src = '../imagens/pagina3-imagem12.svg';
     }
 
     p3BotaoMudarImagem1.addEventListener('click', p3TrocarImagem1);
